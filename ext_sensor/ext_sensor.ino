e9adff7192c53141c8fe2abd0291be56ae0cd9c3
sferraro <sferraro@sfnet>
2018-08-21 00:23:16 -0300

    Control automatico

--- ext_sensor/ext_sensor.ino
+++ ext_sensor/ext_sensor.ino
@@ -19,11 +19,13 @@ int Temp=0;
 int Nivel=1;
 char bufferT[28]="";
 int analog_value = 0;
+boolean Auto[2] = {false,false};
+float Lin[2] = {0,0};
 const char* ssid     = "wifi";
 const char* password = "secret1703secret1703";
 static int Wconectado = 0;
 boolean gpioState[36] = {false,false};
-const char* mqtt_server = "em.isef11.com.ar";
+const char* mqtt_server = "190.2.22.61";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -62,6 +64,22 @@ void Wifi_init(){
   WiFi.onEvent(WiFiEvent);
   WiFi.begin(ssid, password);
 }
+String get_status() {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& data = jsonBuffer.createObject();
+  data[String(RLY_P1)] = gpioState[RLY_P1] ? true : false;
+  data[String(RLY_P2)] = gpioState[RLY_P2] ? true : false;
+  data["AutoP1"] = Auto[0] ? true : false;
+  data["AutoP2"] = Auto[1] ? true : false;
+  data["LinP1"] = Lin[0];
+  data["LinP2"] = Lin[1];
+  char payload[256];
+  data.printTo(payload, sizeof(payload));
+  String strPayload = String(payload);
+  Serial.print("Estado: ");
+  Serial.println(strPayload);
+  return strPayload;
+}
 
 String get_gpio_status() {
   StaticJsonBuffer<200> jsonBuffer;
@@ -80,6 +98,83 @@ void set_gpio_status(int pin, boolean enabled) {
    digitalWrite(pin, enabled ? HIGH : LOW);
    // Update GPIOs state
    gpioState[pin] = enabled;
+}
+
+String get_Auto_status(int pin, String methodName) {
+  //StaticJsonBuffer<200> jsonBuffer;
+  //JsonObject& data = jsonBuffer.createObject();
+  String data =String(Auto[pin] ? true : false);
+  String payload = "{\"";
+      //payload += "\"method\":";
+      payload += methodName;
+      payload += "\":";
+      //payload += "\"params\":";
+      payload += "true";
+      payload += "}";
+
+      // Send payload
+  char attributes[200];
+  payload.toCharArray( attributes,200);
+  
+  
+  //char payload[256];
+  //data.printTo(payload, sizeof(payload));
+  //String strPayload = String(payload);
+  Serial.print("Auto Pin: ");
+  Serial.print(pin);
+  Serial.print(" : ");
+  Serial.print(attributes);
+  Serial.println(".");
+  //return strPayload;
+  return attributes;
+}
+
+void set_Auto_status(int pin, boolean enabled) {
+   Auto[pin] = enabled;
+   Serial.print("Set_Auto :");
+   Serial.print(Auto[pin]);
+   Serial.print(" Pin:");
+   Serial.println(pin);
+}
+
+String get_Lin_Value(int pin, String methodName) {
+  //StaticJsonBuffer<200> jsonBuffer;
+  //JsonObject& data = jsonBuffer.createObject();
+  
+  String data =String(Lin[pin]);
+  String payload = "{";
+      //payload += "\"method\":\"";
+      //payload += methodName;
+      //payload += "\",";
+      //payload += "\"params\":";
+      payload += "\"value\":\"77.50\"";
+      payload += "}";
+
+      // Send payload
+  char attributes[200];
+  payload.toCharArray( attributes,200);
+  
+  //data = Lin[pin];
+  //char payload[256];
+  //data.printTo(payload, sizeof(payload));
+  //String strPayload = String(payload);
+  Serial.print("Lin Pin: ");
+  Serial.print(pin);
+  Serial.print(" : ");
+  //Serial.println(strPayload);
+  //return strPayload;
+  Serial.print(attributes);
+  Serial.println(".");
+  //return strPayload;
+  return attributes;
+}
+
+void set_Lin_Value(int pin, float  valor) {
+   Lin[pin] = valor;
+   Serial.print("Set_Lin :");
+   Serial.print(Lin[pin]);
+   Serial.print(" Pin:");
+   Serial.println(pin);
 }
 
 void on_message(char* topic, byte* payload, unsigned int length) {
@@ -104,45 +199,94 @@ void on_message(char* topic, byte* payload, unsigned int length) {
     Serial.println("parseObject() Error");
     return;
   }
-
+  Serial.println("Data:");
+  data.printTo(Serial);
+  Serial.println("----");
   String methodName = String((const char*)data["method"]);
 
   if (methodName.equals("getGpioStatus") or methodName.equals("getValue")) {
-    String responseTopic = String(topic);
-    responseTopic.replace("request", "response");
-    client.publish(responseTopic.c_str(), get_gpio_status().c_str());
+        String responseTopic = String(topic);
+        responseTopic.replace("request", "response");
+        Serial.print("response :");
+        Serial.println(responseTopic.c_str());
+        Serial.print("datos:");
+        Serial.println(get_gpio_status().c_str());
+        client.publish(responseTopic.c_str(), get_gpio_status().c_str());
+        
   } else if (methodName.equals("setGpioStatus")) {
-    // Update GPIO status and reply
-    set_gpio_status(data["params"]["pin"], data["params"]["enabled"]);
-    String responseTopic = String(topic);
-    responseTopic.replace("request", "response");
-    client.publish(responseTopic.c_str(), get_gpio_status().c_str());
-    client.publish("v1/devices/me/attributes", get_gpio_status().c_str());
-  } else if (methodName.equals("setValueP1")) {
-    set_gpio_status(RLY_P1, data["params"]);
-    String responseTopic = String(topic);
-    responseTopic.replace("request", "response");
-    client.publish(responseTopic.c_str(), data["params"]);
-    client.publish("v1/devices/me/attributes",data["params"]);
-  } else if (methodName.equals("setValueP2")) {
-    set_gpio_status(RLY_P2, data["params"]);
-    String responseTopic = String(topic);
-    responseTopic.replace("request", "response");
-    client.publish(responseTopic.c_str(), data["params"]);
-    client.publish("v1/devices/me/attributes",data["params"]);
-  }
+        // Update GPIO status and reply
+        set_gpio_status(data["params"]["pin"], data["params"]["enabled"]);
+        String responseTopic = String(topic);
+        responseTopic.replace("request", "response");
+        client.publish(responseTopic.c_str(), get_gpio_status().c_str());
+        client.publish("v1/devices/me/attributes", get_status().c_str());
+        
+  } else if(methodName.equals("setAutoP2") or methodName.equals("setAutoP1")) {
+        int indx;
+        if(methodName.equals("setAutoP1")){
+          indx=0;
+        }else{
+          indx=1;
+        }
+        set_Auto_status(indx,data["params"]);        
+        String responseTopic = String(topic);
+        responseTopic.replace("request", "response");
+        Serial.print("response :");
+        Serial.println(responseTopic.c_str());
+        Serial.print("datos:");
+        data.printTo(Serial);
+        client.publish(responseTopic.c_str(), data["params"]);
+        client.publish("v1/devices/me/attributes",get_status().c_str());
+  
+  } else if(methodName.equals("getAutoP2") or methodName.equals("getAutoP1")) {
+        int indx;
+        if(methodName.equals("getAutoP1")){
+          indx=0;
+        }else{
+          indx=1;
+        }
+        String responseTopic = String(topic);
+        responseTopic.replace("request", "response");
+        client.publish(responseTopic.c_str(), get_Auto_status(indx,methodName).c_str());
+         
+  }else if(methodName.equals("setLinP1") or methodName.equals("setLinP2")) {
+        int indx;
+        if(methodName.equals("setLinP1")){
+          indx=0;
+        }else{
+          indx=1;
+        }
+        float val=data["params"];
+        set_Lin_Value(indx,val);
+        String responseTopic = String(topic);
+        responseTopic.replace("request", "response");
+        client.publish(responseTopic.c_str(), data["params"]);
+        client.publish("v1/devices/me/attributes",get_status().c_str());
+        
+  }else if(methodName.equals("getLinP1") or methodName.equals("getLinP2")) {
+        int indx;
+        if(methodName.equals("getLinP1")){
+          indx=0;
+        }else{
+          indx=1;
+        }
+        String responseTopic = String(topic);
+        responseTopic.replace("request", "response");
+        client.publish(responseTopic.c_str(), get_Lin_Value(indx,methodName).c_str());
+  }     
+ 
 }
 
 void send_mqtt(float t, float h, float hic, float co){
   if (Wconectado == 1){
     if(!client.connected()) {
       Serial.print("Conectando ThingsBoard node ...");
-      if ( client.connect("Esp32COSensor", TOKEN_TB, NULL) ) {
+      if ( client.connect("Ext_Sensor_01", TOKEN_TB, NULL) ) {
         Serial.println( "[DONE]" );
         int rsus=client.subscribe("v1/devices/me/rpc/request/+");
         Serial.print( "[SUBSCRIBE]" );
         Serial.println(rsus);
-        client.publish("v1/devices/me/attributes", get_gpio_status().c_str());
+        client.publish("v1/devices/me/attributes", get_status().c_str());
         Serial.print("Enviando GPIO status ...");
         Serial.println(rsus);
       } else {
@@ -193,12 +337,12 @@ void coreTask( void * pvParameters ){
       if(Wconectado == 1){
           if(!client.connected()) {
               Serial.print("Conectando a ThingsBoard node ...");
-              if ( client.connect("Esp32COSensor", TOKEN_TB, NULL) ) {
+              if ( client.connect("Ext_Sensor_01", TOKEN_TB, NULL) ) {
                  Serial.println( "[DONE]" );
                  int rsus=client.subscribe("v1/devices/me/rpc/request/+");
                  Serial.print( "[SUBSCRIBE]" );
                  Serial.println(rsus);
-                 rsus=client.publish("v1/devices/me/attributes", get_gpio_status().c_str()); 
+                 rsus=client.publish("v1/devices/me/attributes", get_status().c_str()); 
                  Serial.print("Enviando GPIO status ...");
                  Serial.println(rsus);
               } else {
